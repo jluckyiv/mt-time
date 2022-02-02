7e6ae5174bd556b4dc274b91c39595cd22aa0b30
Jackson Lucky <jack.lucky.iv@gmail.com>
2022-02-01 21:39:26 -0800

    Update dependencies and move to parcel.js.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import "./main.css";
 import { Elm } from "./Main.elm";
-import registerServiceWorker from "./registerServiceWorker";
 
 const app = Elm.Main.init({
   node: document.getElementById("root"),
-  flags: localStorage.getItem("cache") || ""
+  flags: localStorage.getItem("cache") || "",
 });
 
-app.ports.cache.subscribe(function(data) {
+app.ports.cache.subscribe(function (data) {
   localStorage.setItem("cache", JSON.stringify(data));
 });
 
